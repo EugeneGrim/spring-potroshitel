5eb2d8410a464102aa144be0407d186db9d19a2d
Evgenii Kornaushenkov <evgenii_kornaushenkov@epam.com>
2018-08-27 04:31:02 +0300

    Add custom bean scope 'periodical'

--- src/main/java/screensaver/Config.java
+++ src/main/java/screensaver/Config.java
@@ -10,17 +10,27 @@ import java.util.Random;
 public class Config {
 
     @Bean
-    @Scope("prototype")
+    @Scope("periodical")
     public Color color() {
         Random random = new Random();
         return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
+
+    @Bean
+    public ColorFrame frame() {
+        return new ColorFrame() {
+            @Override
+            protected Color getColor() {
+                return color();
+            }
+        };
     }
 
     public static void main(String[] args) throws InterruptedException {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
         while (true) {
             context.getBean(ColorFrame.class).showOnRandomPlace();
-            Thread.sleep(100);
+            Thread.sleep(150);
         }
     }
 }
